ff8e7663e91ab97b5d65704fe9127a18d1b06f9c
mushamsuresh <sureshmusham51@gmail.com>
2026-03-20 23:24:22 +0530

    initial commit

---
+++ HelloWebServer.java
@@ -0,0 +1,38 @@
+
+import com.sun.net.httpserver.HttpServer;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpExchange;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
+public class HelloWebServer {
+
+    public static void main(String[] args) throws IOException {
+
+        int port = 9090; // changed port here
+
+        HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
+
+        server.createContext("/", new MyHandler());
+
+        server.setExecutor(null);
+        server.start();
+
+        System.out.println("Server started on port " + port);
+    }
+
+    static class MyHandler implements HttpHandler {
+        @Override
+        public void handle(HttpExchange exchange) throws IOException {
+
+            String response = "Hello from DevOps Web App! Running on port 9090";
+
+            exchange.sendResponseHeaders(200, response.length());
+            OutputStream os = exchange.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        }
+    }
+}
